8a30b9994c2099a8c046b98a5d9a45a48e4443e7
JustinFernandes <JustinFernandes@users.noreply.github.com>
2021-03-13 13:21:51 +0530

    toast changed

---
+++ app/src/main/java/com/classmonitor/classmonitor/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.classmonitor.classmonitor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Toast.makeText(this,"hsfddfikj",Toast.LENGTH_SHORT).show()
+
+    }
+}
